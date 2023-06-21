b9284c7f21a00b712fd1d0959c26dc61cc890f21
digantaWeavers <diganta.maity@weavers-web.com>
2023-06-21 19:00:56 +0530

    changing js

--- assets/custom.js
+++ assets/custom.js
@@ -5,3 +5,17 @@ jQuery(document).ready(function(){
     jQuery(this).parent(".accordion-item").nextAll(".accordion-item").find(".accordion-contant").slideUp();
   });
 });
+
+jQuery(document).on('click', '.playbtn', function () {
+  let vid_id = jQuery(this).data("vidid");
+  jQuery('.playbtn').show();
+  jQuery(`#${vid_id}`).trigger("play");
+  jQuery(`.video`).not(`#${vid_id}`).trigger("pause");
+  jQuery(this).hide();
+});
+
+jQuery(document).on('click', '.video', function () {
+  let play_btn_id = jQuery(this).data("play_btn_id");
+  jQuery(this).trigger("pause");
+  jQuery(`#${play_btn_id}`).show();
+});
