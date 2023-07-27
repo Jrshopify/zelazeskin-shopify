99eaf2a3b0c99d2548991e7d1e977d0afee170c8
pritam.badra <pritam.badra@weavers-web.com>
2023-07-27 18:50:31 +0530

    product page design fix

--- assets/custom.js
+++ assets/custom.js
@@ -93,6 +93,24 @@ jQuery('#testimonial').owlCarousel({
   }
 });
 
+jQuery('#product-video-silder').owlCarousel({
+  loop:false,
+  margin:30,
+  nav:true,
+  dots:false,
+  autoplay:true,
+  autoplayTimeout:5000,
+  autoplayHoverPause:true,
+  responsive:{
+      600:{
+          items:2
+      },
+      1000:{
+          items:7
+      }
+  }
+})
+
 /*jQuery(document).on('click', '.playbtn', function () {
 let vid_id = jQuery(this).data("vidid");
 jQuery('.playbtn').show();
