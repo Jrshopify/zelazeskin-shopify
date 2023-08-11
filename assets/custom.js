016c907a9918dab0ff452c507804896981e739a2
digantaWeavers <diganta.maity@weavers-web.com>
2023-08-11 13:35:09 +0530

    hbhdf

--- assets/custom.js
+++ assets/custom.js
@@ -16,11 +16,11 @@ jQuery(document).on('click', '.playbtn', function () {
   let vid_id = jQuery(this).data("vidid");
   jQuery('.playbtn').show();
   jQuery(`#${vid_id}`).trigger("play");
-  jQuery(`.video`).not(`#${vid_id}`).trigger("pause");
+  jQuery(`.close`).not(`#${vid_id}`).trigger("pause");
   jQuery(this).hide();
 });
 
-jQuery(document).on('click', '.video', function () {
+jQuery(document).on('click', '.close', function () {
   let play_btn_id = jQuery(this).data("play_btn_id");
   jQuery(this).trigger("pause");
   jQuery(`#${play_btn_id}`).show();
