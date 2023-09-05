fc2da84de0f406728502410f55bf42374ae895f1
digantaWeavers <diganta.maity@weavers-web.com>
2023-09-05 11:30:58 +0530

    fix

--- assets/custom.js
+++ assets/custom.js
@@ -28,88 +28,104 @@ window.addEventListener("scroll", function () {
         var i = document.querySelector(".mobile-shop-now-button");
         window.scrollY >= 200 ? i.classList.add("visible") : i.classList.remove("visible")
     }
-}), jQuery("#clientfeedback").owlCarousel({
-    loop: false,
-    margin: 30,
-    nav: true,
-    dots: false,
-    autoplay: true,
-    autoplayTimeout: 5000,
-    autoplayHoverPause: true,
-    responsive: {
-        0: {
-            items: 1,
-            margin: 0
-        },
-        600: {
-            items: 2,
-            margin: 10
-        },
-        1e3: {
-            items: 3
-        }
-    }
-}), jQuery("#videoSlide").owlCarousel({
-    loop: false,
-    margin: 30,
-    nav: true,
-    dots: false,
-    autoplay: true,
-    autoplayTimeout: 5000,
-    autoplayHoverPause: true,
-    responsive: {
-        0: {
-            items: 1,
-            margin: 0
-        },
-        600: {
-            items: 2,
-            margin: 10
-        },
-        1e3: {
-            items: 3
-        }
-    }
-}), jQuery("#testimonial").owlCarousel({
-    loop: false,
-    margin: 30,
-    nav: true,
-    dots: false,
-    autoplay: true,
-    autoplayTimeout: 5000,
-    autoplayHoverPause: true,
-    responsive: {
-        0: {
-            items: 1,
-            margin: 0
-        },
-        600: {
-            items: 2,
-            margin: 10
-        },
-        1e3: {
-            items: 3
-        }
-    }
-}), jQuery("#product-video-silder").owlCarousel({
-    loop: false,
-    margin: 20,
-    nav: true,
-    dots: false,
-    autoplay: true,
-    autoplayTimeout: 5000,
-    autoplayHoverPause: true,
-    responsive: {
-        0: {
-            items: 2,
-            margin: 10
-        },
-        991: {
-            items: 4,
-            margin: 10
-        },
-        1e3: {
-            items: 7
-        }
-    }
 });
+
+if("#clientfeedback"){
+    jQuery("#clientfeedback").owlCarousel({
+        loop: false,
+        margin: 30,
+        nav: true,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                margin: 0
+            },
+            600: {
+                items: 2,
+                margin: 10
+            },
+            1e3: {
+                items: 3
+            }
+        }
+    });
+}
+
+if("#videoSlide"){
+    jQuery("#videoSlide").owlCarousel({
+        loop: false,
+        margin: 30,
+        nav: true,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                margin: 0
+            },
+            600: {
+                items: 2,
+                margin: 10
+            },
+            1e3: {
+                items: 3
+            }
+        }
+    });
+} 
+
+if("#testimonial"){
+    jQuery("#testimonial").owlCarousel({
+        loop: false,
+        margin: 30,
+        nav: true,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                margin: 0
+            },
+            600: {
+                items: 2,
+                margin: 10
+            },
+            1e3: {
+                items: 3
+            }
+        }
+    });
+}
+
+if("#testimonial"){
+    jQuery("#product-video-silder").owlCarousel({
+        loop: false,
+        margin: 20,
+        nav: true,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 2,
+                margin: 10
+            },
+            991: {
+                items: 4,
+                margin: 10
+            },
+            1e3: {
+                items: 7
+            }
+        }
+    });
+}
